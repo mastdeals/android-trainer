1b7d32d9c86fadc21717212381458ffa15eb8248
laverdone@gmail.com <laverdone@gmail.com@0eac464e-b049-f64e-da43-062367cf97b6>
2012-11-07 11:55:51 +0000

    Initial Support for GCM

---
+++ AndroidTrainer/src/com/glm/services/GCMIntentService.java
@@ -0,0 +1,35 @@
+package com.glm.services;
+
+import com.google.android.gcm.GCMBaseIntentService;
+
+import android.content.Context;
+import android.content.Intent;
+
+
+public class GCMIntentService extends GCMBaseIntentService{
+	
+	@Override
+	protected void onError(Context context, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onMessage(Context context, Intent arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onRegistered(Context context, String redID) {
+		// TODO Auto-generated method stub
+		 
+	}
+
+	@Override
+	protected void onUnregistered(Context context, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
