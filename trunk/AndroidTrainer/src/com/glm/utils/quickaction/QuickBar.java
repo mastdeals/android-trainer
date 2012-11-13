583ca7a8491aeb0dc52355618c28f0a555bac955
laverdone@gmail.com <laverdone@gmail.com@0eac464e-b049-f64e-da43-062367cf97b6>
2012-11-13 11:04:17 +0000

    UI Improvments

--- trunk/AndroidTrainer/src/com/glm/utils/quickaction/QuickBar.java
+++ trunk/AndroidTrainer/src/com/glm/utils/quickaction/QuickBar.java
@@ -6,34 +6,41 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class QuickBar{
-	private ActionItem nextItem;
-	private ActionItem prevItem;
-	private ActionItem searchItem;
-	private ActionItem infoItem;
-	private ActionItem eraseItem;
-	private ActionItem okItem;
+	private ActionItem mShareItem;	
+	private ActionItem mGraphItem;
+	private ActionItem mMapItem;
+	private ActionItem mEditItem;	
+	private ActionItem mEraseItem;
+	private ActionItem mKMLItem;
+	private ActionItem mGPXItem;
+	private ActionItem mTCXItem;
 	private Context mContext;
 	final QuickAction quickAction;
 	public QuickBar(Context context) {				
 		mContext=context;
 				
-		nextItem     = new ActionItem(1, mContext.getString(R.string.share_long), mContext.getResources().getDrawable(R.drawable.share));
-	    prevItem     = new ActionItem(2, "Prev", mContext.getResources().getDrawable(R.drawable.menu_up_arrow));
-	    searchItem   = new ActionItem(2, "Find", mContext.getResources().getDrawable(R.drawable.menu_search));
-        infoItem     = new ActionItem(3, "Info", mContext.getResources().getDrawable(R.drawable.menu_info));
-        eraseItem    = new ActionItem(4, "Clear", mContext.getResources().getDrawable(R.drawable.menu_eraser));
-        okItem       = new ActionItem(5, "OK", mContext.getResources().getDrawable(R.drawable.menu_ok));
-        prevItem.setSticky(true);
-        nextItem.setSticky(true);
+		mShareItem  = new ActionItem(1, mContext.getString(R.string.share_long), mContext.getResources().getDrawable(R.drawable.share));
+		mGraphItem  = new ActionItem(2, mContext.getString(R.string.exercise_graph), mContext.getResources().getDrawable(R.drawable.graph));
+	    mMapItem    = new ActionItem(3, mContext.getString(R.string.maps), mContext.getResources().getDrawable(R.drawable.map));
+	    mEditItem   = new ActionItem(4, mContext.getString(R.string.edit_note), mContext.getResources().getDrawable(R.drawable.edit));
+        mEraseItem  = new ActionItem(5, mContext.getString(R.string.erase), mContext.getResources().getDrawable(R.drawable.erase));
+        mKMLItem    = new ActionItem(6, "KML", mContext.getResources().getDrawable(R.drawable.export));
+        mGPXItem    = new ActionItem(7, "GPX", mContext.getResources().getDrawable(R.drawable.export));
+        mTCXItem    = new ActionItem(8, "TCX", mContext.getResources().getDrawable(R.drawable.export));
+
+        mMapItem.setSticky(true);
+        mGraphItem.setSticky(true);
         quickAction = new QuickAction(mContext, QuickAction.VERTICAL);
 
         //add action items into QuickAction
-        quickAction.addActionItem(nextItem);
-        quickAction.addActionItem(prevItem);
-        quickAction.addActionItem(searchItem);
-        quickAction.addActionItem(infoItem);
-        quickAction.addActionItem(eraseItem);
-        quickAction.addActionItem(okItem);
+        quickAction.addActionItem(mGraphItem);
+        quickAction.addActionItem(mMapItem);
+        quickAction.addActionItem(mEditItem);
+        quickAction.addActionItem(mShareItem);
+        quickAction.addActionItem(mEraseItem);
+        quickAction.addActionItem(mKMLItem);
+        quickAction.addActionItem(mGPXItem);
+        quickAction.addActionItem(mTCXItem);
         quickAction.setOnActionItemClickListener(new QuickAction.OnActionItemClickListener() {          
             @Override
             public void onItemClick(QuickAction source, int pos, int actionId) {
