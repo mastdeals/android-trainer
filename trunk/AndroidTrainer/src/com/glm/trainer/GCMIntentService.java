a39c22b14e021627383999249b7a01fcbba0351f
laverdone@gmail.com <laverdone@gmail.com@0eac464e-b049-f64e-da43-062367cf97b6>
2012-11-16 14:37:03 +0000

    Fix GCM registration and receive message

--- trunk/AndroidTrainer/src/com/glm/trainer/GCMIntentService.java
+++ trunk/AndroidTrainer/src/com/glm/trainer/GCMIntentService.java
@@ -1,8 +1,12 @@
 package com.glm.trainer;
 
+import com.glm.app.MainTrainerActivity;
 import com.google.android.gcm.GCMBaseIntentService;
 import com.google.android.gcm.GCMRegistrar;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -10,10 +14,11 @@ import android.util.Log;
 
 public class GCMIntentService extends GCMBaseIntentService{
 	private static final String SENDER_ID = "558307532040";
+	private NotificationManager mNM;
 	
 	public GCMIntentService() {
         super(SENDER_ID);
-        Log.i(this.getClass().getCanonicalName(), "Device registered: SENDER_ID = " + SENDER_ID);
+        Log.i(this.getClass().getCanonicalName(), "Device registered: SENDER_ID = " + SENDER_ID);       
     }
 	
 	@Override
@@ -26,6 +31,26 @@ public class GCMIntentService extends GCMBaseIntentService{
 	protected void onMessage(Context context, Intent arg1) {
 		// TODO Auto-generated method stub
 		Log.i(this.getClass().getCanonicalName(), "Device onMessage: = " + arg1);
+		generateNotification(context, arg1.getStringExtra("message"));
+	}
+
+	private void generateNotification(Context context, String stringExtra) {
+		mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE); 	
+
+		// Set the icon, scrolling text and timestamp
+        Notification notification = new Notification(R.drawable.pause_trainer, getText(R.string.app_name_pro),
+                System.currentTimeMillis());
+
+        // The PendingIntent to launch our activity if the user selects this notification Controller
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainTrainerActivity.class), 0);
+
+        // Set the info for the views that show in the notification panel.
+        notification.setLatestEventInfo(this, getText(R.string.app_name_pro),
+        		stringExtra, contentIntent);
+        Log.v(this.getClass().getCanonicalName(), "Show Notification "+stringExtra);
+        // Send the notification.
+        if(mNM!=null) mNM.notify( R.string.app_name_pro, notification);
 	}
 
 	@Override
