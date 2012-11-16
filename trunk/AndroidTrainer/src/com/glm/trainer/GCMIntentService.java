ceb66d70f271d7d68ff1da194884876cfd54f66b
laverdone@gmail.com <laverdone@gmail.com@0eac464e-b049-f64e-da43-062367cf97b6>
2012-11-16 13:58:48 +0000

    Fix GCM registration

--- trunk/AndroidTrainer/src/com/glm/trainer/GCMIntentService.java
+++ trunk/AndroidTrainer/src/com/glm/trainer/GCMIntentService.java
@@ -1,10 +1,11 @@
-package com.glm.services;
+package com.glm.trainer;
 
 import com.google.android.gcm.GCMBaseIntentService;
 import com.google.android.gcm.GCMRegistrar;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 
 public class GCMIntentService extends GCMBaseIntentService{
@@ -12,24 +13,26 @@ public class GCMIntentService extends GCMBaseIntentService{
 	
 	public GCMIntentService() {
         super(SENDER_ID);
+        Log.i(this.getClass().getCanonicalName(), "Device registered: SENDER_ID = " + SENDER_ID);
     }
 	
 	@Override
 	protected void onError(Context context, String arg1) {
 		// TODO Auto-generated method stub
-		
+		Log.i(this.getClass().getCanonicalName(), "Device onError: = " + arg1);
 	}
 
 	@Override
 	protected void onMessage(Context context, Intent arg1) {
 		// TODO Auto-generated method stub
-		
+		Log.i(this.getClass().getCanonicalName(), "Device onMessage: = " + arg1);
 	}
 
 	@Override
 	protected void onRegistered(Context context, String redID) {
 		// TODO Auto-generated method stub
 		GCMRegistrar.setRegisteredOnServer(context, true);
+		Log.i(this.getClass().getCanonicalName(), "Device onRegistered: = " + redID);
 	}
 
 	@Override
